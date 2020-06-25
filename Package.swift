ad6281fa0156b5d222cd736f8675abc077709b83
Moritz Lang <hi@slashmo.codes>
2020-06-25 11:15:27 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "gsoc-swift-baggage-context",
+    products: [
+        .library(name: "Baggage", targets: ["Baggage"])
+    ],
+    targets: [
+        .target(
+            name: "Baggage",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "BaggageTests",
+            dependencies: [
+                "Baggage"
+            ]
+        )
+    ]
+)
