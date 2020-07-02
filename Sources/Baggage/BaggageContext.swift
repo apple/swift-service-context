c6718011ed9ba050a20463e5cf64f8a65034092f
Moritz Lang <hi@slashmo.codes>
2020-07-02 13:42:27 +0200

    Add license headers & check for inappropriate language (#4)
    * Add license header validation script
    
    * Add naming validation script

--- Sources/Baggage/BaggageContext.swift
+++ Sources/Baggage/BaggageContext.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Context Propagation open source project
+//
+// Copyright (c) 2020 Apple Inc. and the Swift Baggage Context project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 /// A `BaggageContext` is a heterogeneous storage type with value semantics for keyed values in a type-safe
 /// fashion. Its values are uniquely identified via `BaggageContextKey`s. These keys also dictate the type of
 /// value allowed for a specific key-value pair through their associated type `Value`.
--- scripts/validate_naming.sh
+++ scripts/validate_naming.sh
@@ -26,28 +26,17 @@
 ##
 ##===----------------------------------------------------------------------===##
 
-set -u
-
-# verify that swiftformat is on the PATH
-command -v swiftformat >/dev/null 2>&1 || { echo >&2 "'swiftformat' could not be found. Please ensure it is installed and on the PATH."; exit 1; }
-
-printf "=> Checking format\n"
-FIRST_OUT="$(git status --porcelain)"
-# swiftformat does not scale so we loop ourselves
-shopt -u dotglob
-find Sources/* Tests/* -type d | while IFS= read -r d; do
-  printf "   * checking $d... "
-  out=$(swiftformat $d 2>&1)
-  SECOND_OUT="$(git status --porcelain)"
-  if [[ "$out" == *"error"*] && ["$out" != "*No eligible files" ]]; then
-    printf "\033[0;31merror!\033[0m\n"
-    echo $out
-    exit 1
-  fi
-  if [[ "$FIRST_OUT" != "$SECOND_OUT" ]]; then
-    printf "\033[0;31mformatting issues!\033[0m\n"
-    git --no-pager diff
-    exit 1
-  fi
-  printf "\033[0;32mokay.\033[0m\n"
-done
+printf "=> Checking for unacceptable language... "
+# This greps for unacceptable terminology. The square bracket[s] are so that
+# "git grep" doesn't find the lines that greps :).
+unacceptable_terms=(
+  -e blacklis[t]
+  -e whitelis[t]
+  -e slav[e]
+)
+if git grep --color=never -i "${unacceptable_terms[@]}" > /dev/null; then
+  printf "\033[0;31mUnacceptable language found.\033[0m\n"
+  git grep -i "${unacceptable_terms[@]}"
+  exit 1
+fi
+printf "\033[0;32mokay.\033[0m\n"
