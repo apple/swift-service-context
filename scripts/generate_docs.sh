36a0fddba7b7b5f0112f1fb4d0f5559b4aad9a71
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2020-12-04 19:54:49 +0900

    fixup docs script

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 ##===----------------------------------------------------------------------===##
 ##
-## This source file is part of the Swift Metrics API open source project
+## This source file is part of the Swift Distributed Tracing Baggage open source project
 ##
-## Copyright (c) 2018-2019 Apple Inc. and the Swift Metrics API project authors
+## Copyright (c) 2020 Apple Inc. and the Swift Distributed Tracing Baggage project authors
 ## Licensed under Apache License v2.0
 ##
 ## See LICENSE.txt for license information
-## See CONTRIBUTORS.txt for the list of Swift Metrics API project authors
 ##
 ## SPDX-License-Identifier: Apache-2.0
 ##
@@ -16,21 +15,24 @@
 set -e
 
 my_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-root_path="$my_path/.."
+declare -r root_path="$my_path/.."
 version=$(git describe --abbrev=0 --tags || echo "0.0.0")
-modules=(CoreMetrics Metrics)
+declare -r modules=(Baggage)
+main_module="Baggage"
+declare -r project_xcodeproj_name="swift-distributed-tracing-baggage"
+declare -r project_gh_name="swift-distributed-tracing-baggage"
 
 if [[ "$(uname -s)" == "Linux" ]]; then
   # build code if required
-  if [[ ! -d "$root_path/.build/x86_64-unknown-linux" ]]; then
+  if [[ ! -d "$root_path/.build/x86_64-unknown-linux-gnu" ]]; then
     swift build
   fi
   # setup source-kitten if required
-  source_kitten_source_path="$root_path/.SourceKitten"
+  source_kitten_source_path="$root_path/.build/.SourceKitten"
   if [[ ! -d "$source_kitten_source_path" ]]; then
     git clone https://github.com/jpsim/SourceKitten.git "$source_kitten_source_path"
   fi
-  source_kitten_path="$source_kitten_source_path/.build/x86_64-unknown-linux/debug"
+  source_kitten_path="$source_kitten_source_path/.build/x86_64-unknown-linux-gnu/debug"
   if [[ ! -d "$source_kitten_path" ]]; then
     rm -rf "$source_kitten_source_path/.swift-version"
     cd "$source_kitten_source_path" && swift build && cd "$root_path"
@@ -39,13 +41,13 @@ if [[ "$(uname -s)" == "Linux" ]]; then
   mkdir -p "$root_path/.build/sourcekitten"
   for module in "${modules[@]}"; do
     if [[ ! -f "$root_path/.build/sourcekitten/$module.json" ]]; then
-      "$source_kitten_path/sourcekitten" doc --spm-module $module > "$root_path/.build/sourcekitten/$module.json"
+      "$source_kitten_path/sourcekitten" doc --spm --module-name $module > "$root_path/.build/sourcekitten/$module.json"
     fi
   done
 fi
 
 [[ -d docs/$version ]] || mkdir -p docs/$version
-[[ -d swift-metrics.xcodeproj ]] || swift package generate-xcodeproj
+[[ -d "$project_xcodeproj_name.xcodeproj" ]] || swift package generate-xcodeproj
 
 # run jazzy
 if ! command -v jazzy > /dev/null; then
@@ -58,26 +60,17 @@ mkdir -p "$jazzy_dir"
 
 module_switcher="$jazzy_dir/README.md"
 jazzy_args=(--clean
-            --author 'SwiftMetrics team'
+            --author 'Swift Distributed Tracing team'
             --readme "$module_switcher"
-            --author_url https://github.com/apple/swift-metrics
-            --github_url https://github.com/apple/swift-metrics
-            --github-file-prefix https://github.com/apple/swift-metrics/tree/$version
+            --author_url https://github.com/apple/$project_gh_name
+            --github_url https://github.com/apple/$project_gh_name
+            --github-file-prefix https://github.com/apple/$project_gh_name/tree/$version
             --theme fullwidth
-            --xcodebuild-arguments -scheme,swift-metrics-Package)
+            --xcodebuild-arguments -scheme,$project_xcodeproj_name-Package)
 cat > "$module_switcher" <<"EOF"
-# SwiftMetrics Docs
+# Swift Distributed Tracing Baggage Docs
 
-SwiftMetrics is a Swift metrics API package.
-
-To get started with SwiftMetrics, [`import Metrics`](../CoreMetrics/index.html). The most important types are:
-
-* [`Counter`](https://apple.github.io/swift-metrics/docs/current/CoreMetrics/Classes/Counter.html)
-* [`Timer`](https://apple.github.io/swift-metrics/docs/current/CoreMetrics/Classes/Timer.html)
-* [`Recorder`](https://apple.github.io/swift-metrics/docs/current/CoreMetrics/Classes/Recorder.html)
-* [`Gauge`](https://apple.github.io/swift-metrics/docs/current/CoreMetrics/Classes/Gauge.html)
-
-SwiftMetrics contains multiple modules:
+This project contains modules:
 EOF
 
 for module in "${modules[@]}"; do
@@ -88,11 +81,17 @@ for module in "${modules[@]}"; do
   echo "processing $module"
   args=("${jazzy_args[@]}" --output "$jazzy_dir/docs/$version/$module" --docset-path "$jazzy_dir/docset/$version/$module"
         --module "$module" --module-version $version
-        --root-url "https://apple.github.io/swift-metrics/docs/$version/$module/")
+        --root-url "https://apple.github.io/$project_gh_name/docs/$version/$module/")
   if [[ -f "$root_path/.build/sourcekitten/$module.json" ]]; then
     args+=(--sourcekitten-sourcefile "$root_path/.build/sourcekitten/$module.json")
   fi
+
   jazzy "${args[@]}"
+
+  if [[ $(cat $root_path/.build/sourcekitten/$module.json) == "0" ]]; then
+    echo "processing module $module ($root_path/.build/sourcekitten/$module.json) yielded EMPTY module json file!"
+    exit 1
+  fi
 done
 
 # push to github pages
@@ -106,7 +105,7 @@ if [[ $PUSH == true ]]; then
   cp -r "$jazzy_dir/docs/$version" docs/
   cp -r "docs/$version" docs/current
   git add --all docs
-  echo '<html><head><meta http-equiv="refresh" content="0; url=docs/current/CoreMetrics/index.html" /></head></html>' > index.html
+  echo "<html><head><meta http-equiv=\"refresh\" content=\"0; url=\"docs/current/$main_module/index.html\" /></head></html>" > index.html
   git add index.html
   touch .nojekyll
   git add .nojekyll
