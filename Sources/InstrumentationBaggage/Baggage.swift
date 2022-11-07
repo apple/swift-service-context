d0a49298dbf2bf3978281ff520099c0946f7a3f6
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2022-11-07 16:52:30 +0900

    Fix missing static keyword (#28)

--- Sources/InstrumentationBaggage/Baggage.swift
+++ Sources/InstrumentationBaggage/Baggage.swift
@@ -242,7 +242,7 @@ extension Baggage {
     /// To access the task-local value, use `Baggage.current`.
     ///
     /// SeeAlso: [Swift Task Locals](https://developer.apple.com/documentation/swift/tasklocal)
-    func withValue<T>(_ value: Baggage?, operation: () throws -> T) rethrows -> T {
+    public static func withValue<T>(_ value: Baggage?, operation: () throws -> T) rethrows -> T {
         try Baggage.$current.withValue(value, operation: operation)
     }
 
@@ -251,7 +251,7 @@ extension Baggage {
     /// To access the task-local value, use `Baggage.current`.
     ///
     /// SeeAlso: [Swift Task Locals](https://developer.apple.com/documentation/swift/tasklocal)
-    func withValue<T>(_ value: Baggage?, operation: () async throws -> T) async rethrows -> T {
+    public static func withValue<T>(_ value: Baggage?, operation: () async throws -> T) async rethrows -> T {
         try await Baggage.$current.withValue(value, operation: operation)
     }
 }
